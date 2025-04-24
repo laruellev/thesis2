01a6d31afe039a357585baf80557a6229dc558b8
vlr <laruellev59.web@gmail.com>
2025-04-24 23:45:00 +0200

    synthetic data generation

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import datetime
+import numpy as np
 import yfinance as yf
+from scipy.stats import norm
+from itertools import product
 
 cwd = '/Users/vlr/PycharmProjects/thesis'
 dl = '/Users/vlr/Downloads/Masters/python'
@@ -46,6 +49,51 @@ for i in range(len(expiry_dates)):
     puts[key]["T"] = str(np.busday_count(start, end))
 puts = pd.concat(puts.values(), ignore_index=True)
 
-puts.to_csv(dl+"/NVDA_puts.csv")
-calls.to_csv(dl+"/NVDA_calls.csv")
-hist.to_csv(dl+"/NVDA_stock.csv")
+'''
+puts.to_csv(dl+"/NVDA_puts_20250425.csv")
+calls.to_csv(dl+"/NVDA_calls_20250425.csv")
+hist.to_csv(dl+"/NVDA_stock_20250425.csv")
+'''
+
+# ATTEMPT AT GENERATING DECENT SYNTHETIC DATA = FAIL #
+
+
+def rem_dupl(lst):
+    return list(dict.fromkeys(lst))
+
+
+def black_scholes_price(S, K, r, T, sigma):  # arbitrage-free price of a call option
+    d1 = (np.log(S / K) + (r + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+    return price
+
+
+K = np.array(rem_dupl(sorted(calls["strike"].values.tolist())))
+sigma = np.array(rem_dupl(sorted(calls["impliedVolatility"].tolist()))[1:])[0::5]
+T = np.array(list(map(int, rem_dupl(calls["T"].values.tolist())[1:])))
+r = np.array([4.32/100])  # https://tradingeconomics.com/united-states/government-bond-yield
+S = np.array([106.43])  # current underlying stock price on Apr 24, 2025
+
+option_prices = pd.DataFrame(
+  product(S, K, r, T, sigma),
+  columns=["S", "K", "r", "T", "sigma"]
+)
+
+option_prices["black_scholes"] = black_scholes_price(
+  option_prices["S"].values,
+  option_prices["K"].values,
+  option_prices["r"].values,
+  option_prices["T"].values,
+  option_prices["sigma"].values
+)
+
+option_prices = (option_prices
+  .assign(
+    observed_price=lambda x: (
+      x["black_scholes"] + np.random.normal(scale=0.5)
+    )
+  )
+)
+
+a = 0
